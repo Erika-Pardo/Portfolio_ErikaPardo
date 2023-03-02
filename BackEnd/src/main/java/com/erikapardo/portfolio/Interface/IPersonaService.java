cf715b4b54e3e0352e0208b2301d3ff553409aa2
Erika-Pardo <erikapardo16@hotmail.com>
2023-03-02 16:21:03 -0300

    Creación del login
    Colgué haciendo los commits anteriores. Luego de terminar cada una de las secciones en el front end, armé el backend con spring boot y netbeans., para poder agregar, modificar y eliminar usuarios.
    Luego creé el login, el cual aún le faltan algunas modificaciones

---
+++ BackEnd/src/main/java/com/erikapardo/portfolio/Interface/IPersonaService.java
@@ -0,0 +1,15 @@
+package com.erikapardo.portfolio.Interface;
+
+import com.erikapardo.portfolio.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer lista de personas
+    public List<Persona> getPersona();
+    //Guardar un objeto de tipo persona
+    public void savePersona (Persona persona);
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona (Long id);
+    //Buscar una persona por ID
+    public Persona findPersona (Long id);
+}
